efd05c861bdfaec09573b7380bee39c854b53ddb
kiboomhan <kiboomhan@gmail.com>
2021-08-27 19:46:39 +0900

    gateway filter 설정

--- src/main/java/com/example/msaapp02/MsaAppController.java
+++ src/main/java/com/example/msaapp02/MsaAppController.java
@@ -1,9 +1,12 @@
 package com.example.msaapp02;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/app02")
 public class MsaAppController {
@@ -11,5 +14,16 @@ public class MsaAppController {
     @GetMapping("/welcome")
     public String welcome() {
         return "App02 : Hello World";
+    }
+
+    @GetMapping("message")
+    public String message(@RequestHeader("app02-gate-in") String gate) {
+        log.info("app02-gate-in : {}", gate);
+        return "Hello World in app02";
+    }
+
+    @GetMapping("check")
+    public String check() {
+        return "check in app02";
     }
 }
